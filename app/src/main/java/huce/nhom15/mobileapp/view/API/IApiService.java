d2d6e9c61185904e697da50fb9d107422a24b431
hiep <hiep68965@huce.edu.vn>
2023-11-25 20:31:32 +0700

    Hiep commit search and category

--- app/src/main/java/huce/nhom15/mobileapp/view/API/IApiService.java
+++ app/src/main/java/huce/nhom15/mobileapp/view/API/IApiService.java
@@ -5,11 +5,13 @@ import com.google.gson.GsonBuilder;
 
 import java.util.List;
 
+import huce.nhom15.mobileapp.view.viewmodel.CategoryViewModel;
 import huce.nhom15.mobileapp.view.viewmodel.SanPhamViewModel;
 import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface IApiService {
     Gson gson = new GsonBuilder().setDateFormat("dd-MM-yyyy").create();
@@ -21,4 +23,10 @@ public interface IApiService {
 
     @GET("getListSP.php")
     Call<List<SanPhamViewModel>> getListSanPham();
+    @GET("searchSPByName.php")
+    Call<List<SanPhamViewModel>> getListSanPhamSearch(@Query("tensp") String tensp, @Query("page") int page);
+    @GET("getListCategory.php")
+    Call<List<CategoryViewModel>> getListCategory();
+    @GET("getListProductCategory.php")
+    Call<List<SanPhamViewModel>> getListProductCategory(@Query("LSP_MaLoai") int maLoai, @Query("page") int page);
 }
---
+++ app/src/main/java/huce/nhom15/mobileapp/view/activity/SearchActivity.java
@@ -0,0 +1,192 @@
+package huce.nhom15.mobileapp.view.activity;
+
+import static huce.nhom15.mobileapp.R.drawable.product_notfound;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+import huce.nhom15.mobileapp.R;
+import huce.nhom15.mobileapp.view.API.IApiService;
+import huce.nhom15.mobileapp.view.PaginationScrollListener;
+import huce.nhom15.mobileapp.view.adapter.SanPhamSearchAdapter;
+import huce.nhom15.mobileapp.view.viewmodel.SanPhamViewModel;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class SearchActivity extends AppCompatActivity {
+    private String tensp;
+    private EditText edtsearch;
+    private SanPhamSearchAdapter sanPhamSearchAdapter;
+    private List<SanPhamViewModel> sanphams;
+    private Context ct;
+    private boolean isLoading;
+    private boolean isLastPage;
+    private int currentPage = 1;
+    private Handler handler;
+    private RecyclerView rcvSearch;
+    private CircleImageView circleImageView;
+    private ProgressBar progressBar;
+    private boolean productNotFound = true;
+    private ScrollView scrollView;
+    private ImageView pNotFound;
+    private FrameLayout foundProduct;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+        khoiTao();
+        back();
+        tensp = getIntent().getStringExtra("tensp");
+        edtsearch.setText(tensp);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2);
+        rcvSearch.setLayoutManager(gridLayoutManager);
+        ct = this;
+        callApi(tensp, currentPage);
+        editorEditText();
+        recycleViewScroll(gridLayoutManager);
+    }
+    private void recycleViewScroll(GridLayoutManager gridLayoutManager) {
+        rcvSearch.clearOnScrollListeners();
+        rcvSearch.addOnScrollListener(new PaginationScrollListener(gridLayoutManager) {
+            @Override
+            public void loadMoreItem() {
+                isLoading = true;
+                currentPage+=1;
+                loadNextPage(tensp, currentPage);
+            }
+            @Override
+            public boolean isLoading() {
+                return isLoading;
+            }
+
+            @Override
+            public boolean isLastPage() {
+                return isLastPage;
+            }
+        });
+    }
+
+    private void editorEditText() {
+        edtsearch.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE || (event != null && event.getKeyCode() == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN)) {
+                    InputMethodManager imm = (InputMethodManager) edtsearch.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(edtsearch.getWindowToken(), 0);
+                    currentPage = 1;
+                    isLoading=false;
+                    isLastPage=false;
+                    productNotFound = true;
+                    pNotFound.setVisibility(View.GONE);
+                    scrollView.setVisibility(View.VISIBLE);
+                    if(sanphams != null){
+                        sanphams.clear();
+                        sanPhamSearchAdapter.notifyDataSetChanged();
+                    }
+
+                    tensp = edtsearch.getText().toString();
+
+                    callApi(tensp, currentPage);
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+    private void loadNextPage(String tensp, int currentPage){
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                IApiService.api.getListSanPhamSearch(tensp, currentPage).enqueue(new Callback<List<SanPhamViewModel>>() {
+                    @Override
+                    public void onResponse(Call<List<SanPhamViewModel>> call, Response<List<SanPhamViewModel>> response) {
+                        sanPhamSearchAdapter.removeFooterLoading();
+                        List<SanPhamViewModel> list = response.body();
+                        sanphams.addAll(list);
+                        sanPhamSearchAdapter.notifyDataSetChanged();
+                        isLoading=false;
+                        sanPhamSearchAdapter.addFooterLoading();
+                    }
+                    @Override
+                    public void onFailure(Call<List<SanPhamViewModel>> call, Throwable t) {
+                        isLastPage = true;
+                        sanPhamSearchAdapter.removeFooterLoading();
+                        Toast.makeText(ct, "Đã load hết dữ liệu", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        }, 2000);
+    }
+
+    private void callApi(String tensp, int currentPage) {
+        progressBar.setVisibility(View.VISIBLE);
+        IApiService.api.getListSanPhamSearch(tensp, currentPage).enqueue(new Callback<List<SanPhamViewModel>>() {
+            @Override
+            public void onResponse(Call<List<SanPhamViewModel>> call, Response<List<SanPhamViewModel>> response) {
+                progressBar.setVisibility(View.INVISIBLE);
+                sanphams = response.body();
+                sanPhamSearchAdapter = new SanPhamSearchAdapter(sanphams, ct);
+                rcvSearch.setAdapter(sanPhamSearchAdapter);
+                sanPhamSearchAdapter.addFooterLoading();
+                productNotFound = false;
+            }
+
+            @Override
+            public void onFailure(Call<List<SanPhamViewModel>> call, Throwable t) {
+                progressBar.setVisibility(View.INVISIBLE);
+                isLastPage = true;
+                if(productNotFound){
+
+                    pNotFound.setVisibility(View.VISIBLE);
+                    pNotFound.setImageResource(product_notfound);
+                    scrollView.setVisibility(View.GONE);
+                    sanPhamSearchAdapter = new SanPhamSearchAdapter();
+                    rcvSearch.setAdapter(sanPhamSearchAdapter);
+                }
+            }
+        });
+    }
+
+    private void back() {
+        circleImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    private void khoiTao() {
+        edtsearch = findViewById(R.id.edtsearch);
+        rcvSearch = findViewById(R.id.rcv_TC);
+        circleImageView = findViewById(R.id.imglogo);
+        progressBar = findViewById(R.id.progressBar_TC);
+        scrollView = findViewById(R.id.scrollView);
+        pNotFound = findViewById(R.id.productnotfound);
+        foundProduct = findViewById(R.id.foundproduct);
+    }
+}
