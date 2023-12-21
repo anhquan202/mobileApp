ed94daab183714df88c56850e752df5910b8b87d
AnhQuan202 <120768861+anhquan1812@users.noreply.github.com>
2023-12-21 11:25:14 +0700

    day lai IApiServices

--- app/src/main/java/huce/nhom15/mobileapp/view/API/IApiService.java
+++ app/src/main/java/huce/nhom15/mobileapp/view/API/IApiService.java
@@ -3,13 +3,15 @@ package huce.nhom15.mobileapp.view.API;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-import java.util.Currency;
 import java.util.List;
 
 import huce.nhom15.mobileapp.model.Customer;
 import huce.nhom15.mobileapp.view.ModelRespone.LoginRespone;
 import huce.nhom15.mobileapp.view.ModelRespone.RegisterRespone;
+import huce.nhom15.mobileapp.viewmodel.CategoryViewModel;
+import huce.nhom15.mobileapp.viewmodel.ChiTietSPViewModel;
 import huce.nhom15.mobileapp.view.ModelRespone.SetNewPassRespone;
+import huce.nhom15.mobileapp.viewmodel.OrderViewModel;
 import huce.nhom15.mobileapp.viewmodel.SanPhamViewModel;
 import retrofit2.Call;
 import retrofit2.Retrofit;
@@ -28,14 +30,29 @@ public interface IApiService {
             .build()
             .create(IApiService.class);
 
+
     @GET("getListSP.php")
     Call<List<SanPhamViewModel>> getListSanPham();
+
+    @GET("searchSPByName.php")
+    Call<List<SanPhamViewModel>> getListSanPhamSearch(@Query("tensp") String tensp, @Query("page") int page);
+    @GET("getListCategory.php")
+    Call<List<CategoryViewModel>> getListCategory();
+    @GET("getListProductCategory.php")
+    Call<List<SanPhamViewModel>> getListProductCategory(@Query("LSP_MaLoai") int maLoai, @Query("page") int page);
+
+    @GET("getThongTinCTSP.php")
+    Call<ChiTietSPViewModel> getThongTinCTSP(@Query("SP_MaSP") int maSP);
+    @GET("getOrder.php")
+    Call<List<OrderViewModel>> getOrder(@Query("KH_MaKH") int KH_MaKH);
+    @GET("getUser.php")
+    Call<Customer> getUser(@Query("KH_Email") String KH_Email);
     @FormUrlEncoded
     @POST("login.php")
     Call<LoginRespone> login(
             @Field("KH_Email") String email,
             @Field("KH_Password") String password
-            );
+    );
 
     @FormUrlEncoded
     @POST("register.php")
@@ -52,6 +69,21 @@ public interface IApiService {
             @Field("KH_Email") String email,
             @Field("KH_Password") String password
     );
-    @GET("searchSPByName.php")
-    Call<List<SanPhamViewModel>> getListSanPhamSearch(String tensp, int currentPage);
+
+    @FormUrlEncoded
+    @POST("addHoaDon.php")
+    Call<Integer> addHoaDon(
+            @Field("KH_MaKH") int KH_MaKH,
+            @Field("TenNguoiDat") String TenNguoiDat,
+            @Field("TenNguoiNhan") String TenNguoiNhan,
+            @Field("SDT") String SDT,
+            @Field("DiaChi") String DiaChi
+    );
+    @FormUrlEncoded
+    @POST("ThanhToan.php")
+    Call<String> ThanhToan(
+            @Field("MaHD") int MaHD,
+            @Field("MangGioHang") String MangGioHang
+    );
+
 }
